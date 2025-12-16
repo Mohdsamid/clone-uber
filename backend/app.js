83da35ca0e85fc8c788433208203848f7fd40e30
MohdSamid <samidalishah0123@gmail.com>
2025-12-17 00:47:12 +0530

    mongo setup

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const connectTodb = require('./db/db');
+
+connectTodb();
 
 app.use(cors());
 
